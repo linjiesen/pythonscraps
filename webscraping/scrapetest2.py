e752cfb41308b15ed4cd12eb30339e161f4622bb
Aurora <347625023@qq.com>
2017-03-10 22:30:43 +0800

    Create scrapetest2.py

---
+++ webscraping/scrapetest2.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+def getTitle(url):
+        try:
+            html=urlopen(url)
+        except HTTPError as e:
+            return None
+        try:
+            bs0bj=BeautifulSoup(html.read())
+            title=bs0bj.body.h1
+        except AttributeError as e:
+            return None
+        return title
+title=getTitle("http://pythonscraping.com/pages/page1.html")
+if title==None:
+    print("Title could not be found")
+else:
+    print(title)
