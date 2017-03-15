6a18f0064a3dcac107615e87637a0df2fa05bf75
Aurora <347625023@qq.com>
2017-03-15 22:51:36 +0800

    Add files via upload
    为了避免一个页面被采集两次,链接去重是非常重要的。在代码运行时,把已发现的所有链接都放到一起,并保存在方便查询的列表里(下文示例指 Python 的集合 set 类型)。只有“新”链接才会被采集,之后再从页面中搜索其他链接

---
+++ webscraping/hreftest3.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages=set()
+def getLinks(pageUrl):
+    global pages
+    html=urlopen("http://en.wikipedia.org"+pageUrl)
+    bs0bj=BeautifulSoup(html,"lxml")
+    for link in bs0bj.findAll("a",href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage=link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks("")
