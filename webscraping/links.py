319dc4b6f8dcef98fe49f69793cdcd5ff6e29a91
Aurora <347625023@qq.com>
2017-03-18 22:41:58 +0800

    Add files via upload
    网上方法解决的

--- webscraping/links.py
+++ webscraping/links.py
@@ -1,45 +1,59 @@
 from urllib.request import urlopen
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import re
 import datetime
 import random
 
-pages=set()
+pages = set()
 random.seed(datetime.datetime.now())
 
-def getInternalLinks(bs0bj,includeUrl):
-    internalLinks=[]
-    for link in bs0bj.findAll("a",href=re.compile("^(/|.*"+includeUrl+")")):
+
+# Retrieves a list of all Internal links found on a page
+def getInternalLinks(bsObj, includeUrl):
+    includeUrl = urlparse(includeUrl).scheme + "://" + urlparse(includeUrl).netloc
+    internalLinks = []
+    # Finds all links that begin with a "/"
+    for link in bsObj.findAll("a", href=re.compile("^(/|.*" + includeUrl + ")")):
         if link.attrs['href'] is not None:
             if link.attrs['href'] not in internalLinks:
-                internalLinks.append(link.attrs['href'])
+                if (link.attrs['href'].startswith("/")):
+                    internalLinks.append(includeUrl + link.attrs['href'])
+                else:
+                    internalLinks.append(link.attrs['href'])
     return internalLinks
 
-def getExternalLinks(bs0bj,excludeUrl):
-    externalLinks=[]
-    for link in bs0bj.findAll("a",href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+
+# Retrieves a list of all external links found on a page
+def getExternalLinks(bsObj, excludeUrl):
+    externalLinks = []
+    # Finds all links that start with "http" or "www" that do
+    # not contain the current URL
+    for link in bsObj.findAll("a", href=re.compile(
+                            "^(http|www)((?!" + excludeUrl + ").)*$")):
         if link.attrs['href'] is not None:
             if link.attrs['href'] not in externalLinks:
                 externalLinks.append(link.attrs['href'])
     return externalLinks
 
-def splitAddress(adress):
-    addressParts=address.replace("http://","").split("/")
-    return addressParts
 
 def getRandomExternalLink(startingPage):
-    html=urlopen(startingPage)
-    bs0bj=BeautifulSoup(html,"lxml")
-    externalLinks=getExternalLinks(bs0bj,splitAddress(startingPage)[0])
-    if len(ExternalLinks)==0:
-        internalLinks=getInternalLinks(startingPage)
-        return getNextExternalLink(internalLinks[random.randint(0,len(internalLinks)-1)])
+    html = urlopen(startingPage)
+    bsObj = BeautifulSoup(html, "html.parser")
+    externalLinks = getExternalLinks(bsObj, urlparse(startingPage).netloc)
+    if len(externalLinks) == 0:
+        print("No external links, looking around the site for one")
+        domain = urlparse(startingPage).scheme + "://" + urlparse(startingPage).netloc
+        internalLinks = getInternalLinks(bsObj, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks) - 1)])
     else:
-        return externalLinks[random.randint(0,len(externalLinks)-1)]
+        return externalLinks[random.randint(0, len(externalLinks) - 1)]
+
 
 def followExternalOnly(startingSite):
-    externalLink=getRandomExternalLink("http://oreilly.com")
-    print("随机外链是:"+externalLink)
+    externalLink = getRandomExternalLink(startingSite)
+    print("Random external link is: " + externalLink)
     followExternalOnly(externalLink)
+
 
 followExternalOnly("http://oreilly.com")
